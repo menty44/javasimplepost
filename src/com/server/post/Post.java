275ea24687930eb407243a44f706c0072e7c5efb
fred <fred.oluoch@impalapay.com>
2017-02-16 08:37:27 +0300

    version one

---
+++ src/com/server/post/Post.java
@@ -0,0 +1,35 @@
+package com.server.post;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class Post {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("http://localhost/server/example.php/Example?method=sayHello");
+        Map<String,Object> params = new LinkedHashMap<>();
+        params.put("name", "Oluoch");
+        params.put("age", "99");
+        
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+        for (int c; (c = in.read()) >= 0;)
+            System.out.print((char)c);
+    }
+}
